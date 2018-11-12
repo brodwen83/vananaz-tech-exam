2f9eb435aa700aa4bb48753aa0e8046879b12b1f
wendell <brodwen83@gmail.com>
2018-11-12 15:08:00 +0800

    Applied validation to the password field

--- src/components/Auth/LogInForm.js
+++ src/components/Auth/LogInForm.js
@@ -4,19 +4,57 @@ import {
   View,
   StyleSheet,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert
 } from "react-native";
 
 export class LogInForm extends Component {
   state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
     errors: {
-      email: "email errors here",
-      password: "password error here"
+      // email: "email errors here",
+      // password: "password error here"
     }
   };
+
+  handleChange = (holder, text) => {
+    this.setState({
+      data: { ...this.state.data, [holder]: text }
+    });
+  };
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!data.password) {
+      errors.password = "Password can't be blank";
+      return errors;
+    }
+    if (data.password.length < 6 || data.password.length > 12)
+      errors.password = "please use at least 6 - 12 characters";
+
+    return errors;
+  };
+
+  displayFormState = () => {
+    Alert.alert("Login form states", "Tha data: ", [
+      { text: `Email: ${this.state.data.email}` },
+      { text: this.state.data.password }
+    ]);
+  };
+
   render() {
     const {
-      errors: { email, password }
+      data: { email, password },
+      errors
     } = this.state;
     return (
       <View style={[styles.stretch, styles.formContainer]}>
@@ -26,8 +64,9 @@ export class LogInForm extends Component {
             style={styles.textInput}
             underlineColorAndroid={"transparent"}
             placeholder="Input email address"
+            onChangeText={text => this.handleChange("email", text)}
           />
-          <Text style={styles.textError}>{email}</Text>
+          <Text style={styles.textError}>{errors.email}</Text>
         </View>
         <View style={styles.formGroup}>
           <Text style={styles.label}>Password</Text>
@@ -36,11 +75,16 @@ export class LogInForm extends Component {
             style={[styles.stretch, styles.textInput]}
             underlineColorAndroid={"transparent"}
             placeholder="Input password"
+            onChangeText={text => this.handleChange("password", text)}
           />
-          <Text style={styles.textError}>{password}</Text>
+          <Text style={styles.textError}>{errors.password}</Text>
         </View>
         <View style={styles.formGroup}>
-          <TouchableOpacity style={[styles.stretch, styles.button]}>
+          <TouchableOpacity
+            // onPress={this.displayFormState}
+            onPress={this.onSubmit}
+            style={[styles.stretch, styles.button]}
+          >
             <Text style={styles.signInBtnText}>Sign in</Text>
           </TouchableOpacity>
         </View>
